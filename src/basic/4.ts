40f8ebf35f82207aecfddff3bdf56f09fec70863
mrs_misteriya <nata.nedostupnaya@mail.com>
2024-05-26 17:50:06 +0300

    create structure

---
+++ src/basic/4.ts
@@ -0,0 +1,17 @@
+// У вас є такі функції JavaScript:
+
+// function showMessage(message) {
+//   console.log(message);
+// }
+
+// function calc(num1, num2) {
+//   return num1 + num2;
+// }
+
+// function customError() {
+//   throw new Error('Error');
+// }
+
+
+
+// Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
---
+++ src/basic/6.ts
@@ -0,0 +1,17 @@
+// Створіть інтерфейс User для типізації об'єктів, які містять такі властивості. Зверніть увагу, що адреса є необов'язковою властивістю.
+
+// const mango = {
+//     name: 'Mango',
+//     age: 30,
+//     email: 'john@example.com',
+//     address: {
+//         city: 'New York',
+//         country: 'USA'
+//     }
+// };
+
+// const poly = {
+//     name: 'Mango',
+//     age: 30,
+//     email: 'john@example.com'
+// };
